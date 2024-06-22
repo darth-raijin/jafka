1c1a8230ea686c875ebe1840f5fedbc849762819
Mohamed Macow <mmacovv@gmail.com>
2024-06-22 11:55:18 +0200

    init

---
+++ src/main/java/dk/raijin/jafka/JafkaApplication.java
@@ -0,0 +1,13 @@
+package dk.raijin.jafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/raijin/jafka/JafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.raijin.jafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
